f22a485ea368cc7b1ffcae5ce71ee49d9ad49f32
DPlazonic <66380253+DPlazonic@users.noreply.github.com>
2021-05-19 22:07:50 +0200

    Add files via upload

---
+++ happy_shop/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
