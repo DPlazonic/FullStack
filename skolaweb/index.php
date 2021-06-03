4603135ad62d211b7faba1bd26cf5034b2561de0
DPlazonic <66380253+DPlazonic@users.noreply.github.com>
2021-06-03 20:18:56 +0200

    Add files via upload

---
+++ skolaweb/index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
+
+    <title>Skola veb dizajna</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- nav -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Naslovna <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">O nama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Novosti</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kursevi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Prodavnica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- header -->
+    <header class="mainHeader">
+        <article class="container d-flex justify-content-center flex-column text-white text-center">
+            <div class="mb-5">
+                <h1 class="display-4">Škola veb dizajna </h1>
+                <p class="lead">će vam pomoći da za najkraće vreme <br> postanete veb dizajner </p>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h2>HTML i CSS</h2>
+                    <a href="">Saznaj više &gt;</a>
+                </div>
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h2>WordPress</h2>
+                    <a href="">Saznaj više &gt;</a>
+                </div>
+                <div class="col-md-4 p-4 rounded-lg">
+                    <h2>Javascript i PHP</h2>
+                    <a href="">Saznaj više &gt;</a>
+                </div>
+            </div>
+        </article>
+    </header>
+
+    <footer>
+        <article class="container">
+            <div>
+                <img src="img/logo.png" alt="" class="img-fluid">
+            </div>
+            <div>
+                <h4>Partneri</h4>
+                <ul class="list-unstyled">
+                    <li><a href="https://resursnicentar.org.rs/">Resursni cebtar</a></li>
+                    <li><a href="https://bankahrane.rs/">Banka Hrane</a></li>
+                    <li><a href="https://www.danilovesovic.com/">danilovesovic.com</a></li>
+                </ul>
+            </div>
+        </article>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
